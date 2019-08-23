49069aadd0c27769bd896643082eb558443aa578
Kapsner <lorenz.kapsner@uk-erlangen.de>
2019-08-23 14:51:54 +0200

    augmented to v0.0.1.9001

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -15,7 +15,7 @@ my_desc$set("Copyright", "Universitätsklinikum Erlangen")
 # Remove some author fields
 my_desc$del("Maintainer")
 # Set the version
-my_desc$set_version("0.0.1.9000")
+my_desc$set_version("0.0.1.9001")
 # The title of your package
 my_desc$set(Title = "DQAstats - Data Quality Assesment Utilities Package")
 # The description of your package
