61f1ebf1eb43ef1210bfa5f98f818189df6c1444
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2020-04-29 16:57:53 +0200

    modified debug info

--- data-raw/debugging_info.R
+++ data-raw/debugging_info.R
@@ -9,10 +9,11 @@
 devtools::load_all()
 ## using real data:
 #source_system_name = "i2b2"
-#source_system_name = "p21csv"
-source_system_name = "p21staging"
+source_system_name = "p21csv"
+#source_system_name = "p21staging"
 #source_system_name = "fhirgw"
 target_system_name = "i2b2"
+#target_system_name = "p21csv"
 #target_system_name = "p21staging"
 #target_system_name = "omop"
 # config_file = "../demo_settings_INTERNAL.yml"
