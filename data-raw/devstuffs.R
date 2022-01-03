6f551b8f149b791ec9805ec229e3a377d36e78a0
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-01-03 16:20:59 +0100

    chore: added creation of news.md to devstuffs.R

--- data-raw/devstuffs.R
+++ data-raw/devstuffs.R
@@ -157,3 +157,10 @@ usethis::use_git_ignore(".~lock.*.csv#")
 
 
 usethis::use_tidy_description()
+
+# create NEWS.md using the python-package "auto-changelog" (must be installed)
+# https://www.conventionalcommits.org/en/v1.0.0/
+# build|ci|docs|feat|fix|perf|refactor|test
+system(
+  command = 'auto-changelog -u -t "DQAstats NEWS" --tag-prefix "v" -o "NEWS.md"'
+)
