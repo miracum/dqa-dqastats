64377bf9423b813d4f4a95944f337f2d8c3cc029
Lorenz Kapsner <lorenz.kapsner@uk-erlangen.de>
2022-05-17 21:52:38 +0200

    test: removed contexts

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,5 +5,4 @@ library(utils)
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c("logfile_dir"))
 }
-
 test_check("DQAstats")
--- tests/testthat/test-DQA.R
+++ tests/testthat/test-DQA.R
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-context("test DQA function")
-
 library(data.table)
 
 test_that("correct functioning of DQA", {
