8d45355c8916c03eaf0d9cdce04b48c04d39947f
kapsner <lorenz.kapsner@gmail.com>
2025-02-05 13:10:35 +0100

    feat: adding thread limit for cran checks

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,9 @@
 #' @importFrom magrittr "%>%"
 #
 NULL
+
+# https://stackoverflow.com/a/77323812
+.onLoad <- function(libname, pkgname) {
+  # CRAN OMP THREAD LIMIT
+  Sys.setenv("OMP_THREAD_LIMIT" = 2)
+}
