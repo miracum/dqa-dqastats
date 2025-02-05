6bba532e3f610e67487f310e3a116d7353659aba
kapsner <lorenz.kapsner@gmail.com>
2025-02-05 15:29:15 +0100

    fix: another try not going parallel on cran

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,3 @@
 #' @importFrom magrittr "%>%"
 #
 NULL
-
-# https://stackoverflow.com/a/77323812
-.onLoad <- function(libname, pkgname) {
-  # CRAN OMP THREAD LIMIT
-  Sys.setenv("OMP_THREAD_LIMIT" = 2)
-}
--- tests/testthat.R
+++ tests/testthat.R
@@ -5,4 +5,6 @@ library(utils)
 if (getRversion() >= "2.15.1") {
   utils::globalVariables(c("logfile_dir"))
 }
+# https://github.com/Rdatatable/data.table/issues/5658
+Sys.setenv("OMP_THREAD_LIMIT" = 2)
 test_check("DQAstats")
